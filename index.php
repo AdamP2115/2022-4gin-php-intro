d786a27b45bb6f0c33c74f6ec793e7adcaf6f077
AdamP2115 <95406196+AdamP2115@users.noreply.github.com>
2022-09-13 10:33:55 +0200

    Zadanie2.2

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
-$content = file_get_contents('https://www.lipsum.com/feed/html');
+$content = file_get_contents('https://www.loripsum.net/api');
 $array = explode(' ', $content);
-var_dump($array);
+
+echo("W porządku alfabetycznym: ");
+$i=1;
+$search = "";
+sort($array, SORT_NATURAL | SORT_FLAG_CASE);
+foreach($array as $word){
+    if(preg_match("/\b(\w*$search\w*)\b/", $word, $match) == true){
+    echo "[$i] $match[0] </br>";
+    $i ++;
+    }
+}
 ?>
