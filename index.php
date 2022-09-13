0238ee38b76f7e693c3af47a33887bec7b889b30
AdamP2115 <95406196+AdamP2115@users.noreply.github.com>
2022-09-13 11:02:27 +0200

    Zadanie 2.1

--- index.php
+++ index.php
@@ -2,14 +2,10 @@
 $content = file_get_contents('https://www.loripsum.net/api');
 $array = explode(' ', $content);
 
-echo("W porządku alfabetycznym: ");
-$i=1;
-$search = "";
-sort($array, SORT_NATURAL | SORT_FLAG_CASE);
-foreach($array as $word){
-    if(preg_match("/\b(\w*$search\w*)\b/", $word, $match) == true){
-    echo "[$i] $match[0] </br>";
-    $i ++;
+echo("Slowa zawierajace litere e: ");
+foreach($array as $slowanae){
+    if(preg_match('/\b(\w*e\w*)\b/', $slowanae, $match) == true){
+    echo "$match[0] </br>";
     }
 }
 ?>
