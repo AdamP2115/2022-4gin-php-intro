e2e1e7ae556f46ca9cfb9a4c365921d9318fafb3
AdamP2115 <95406196+AdamP2115@users.noreply.github.com>
2022-09-21 11:27:01 +0200

    Dopiero zaczete 4

--- smigol.php
+++ smigol.php
@@ -3,30 +3,38 @@ $content = file_get_contents('https://www.loripsum.net/api');
 $array = explode(' ', $content);
 $kolumny = 1;
 function renderHTMLTable($array, $kolumny, $liczba, $znajdz){
-    foreach($array as $znajdz){
-        if(preg_match('/\b(\w*e\w*)\b/', $znajdz) == true){
-        }
-    }
     echo "<table>";
-    foreach($array as $word){
+    foreach($array as $znajdz){
+        if(preg_match('/\b(\w*e\w*)\b/', $znajdz) == true);
         if($kolumny<=$liczba){
         if ($kolumny%$liczba==1){
-            echo "<tr><th>$word</th>";
+            echo "<tr><th>$znajdz</th>";
         } else if ($kolumny%$liczba==0){
-            echo "<th>$word</th></tr>";
+            echo "<th>$znajdz</th></tr>";
         } else {
-            echo "<th>$word</th>";
+            echo "<th>$znajdz</th>";
         }} else {
         if ($kolumny%$liczba==1){
-            echo "<tr><td>$word</td>";
+            echo "<tr><td>$znajdz</td>";
         } else if ($kolumny%$liczba==0){
-            echo "<td>$word</td></tr>";
+            echo "<td>$znajdz</td></tr>";
         } else {
-            echo "<td>$word</td>";
+            echo "<td>$znajdz</td>";
         }}
         $kolumny ++;
     };
     echo "</table>";
 };
 renderHTMLTable($array, $kolumny, $_POST["liczba"], $_POST["znajdz"]);
+
+function renderCSV($array, $filename = "export.csv", $delimiter=";") {
+    header('Content-Type: application/csv');
+    header('Content-Disposition: attachment; filename="'.$filename.'";');
+
+    $f = fopen('php://output', 'w');
+
+    foreach ($array as $line) {
+        fputcsv($f, $line, $delimiter);
+    }
+}
 ?>
